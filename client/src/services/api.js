c25be4ac689e37c3ea5610c2f5817b2f8d7f1491
Debanjali081 <debanjali017@gmail.com>
2025-07-20 17:13:52 +0530

    render url added

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000", // backend URL
+  baseURL: "https://cactro-fsd-test.onrender.com", // backend URL
   withCredentials: true,
 });
 
